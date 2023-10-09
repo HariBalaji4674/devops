6303ea62afe82b223f90c387eaaa0120a4f322c1
HariBalaji4674 <146502109+HariBalaji4674@users.noreply.github.com>
2023-10-09 10:55:53 +0530

    Delete ShellScripts directory

--- ShellScripts/Second.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-name1=peddireddy
-name2=harivardhan
-
-echo "${name1} is my Family Name My parent kept"
-echo "${name2} is my original Name You can call hari"
--- ShellScripts/loops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#changed Format in the file 
-num1=1
-num2=10
-
-for i in [$num1..$num2]
-do 
-    echo $i
-done
